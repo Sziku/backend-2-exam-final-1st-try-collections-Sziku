2028a226b0dab83d4ec27448294a163a1c043e7d
Sziku <105989158+Sziku@users.noreply.github.com>
2022-12-13 12:20:16 +0100

    try refactor the code

--- src/main/java/hu/nive/ujratervezes/spamfiltering/SpamFilter.java
+++ src/main/java/hu/nive/ujratervezes/spamfiltering/SpamFilter.java
@@ -3,32 +3,36 @@ package hu.nive.ujratervezes.spamfiltering;
 import java.util.*;
 
 public class SpamFilter {
-    private List<String> whitelist;
-    private List<String> blacklist;
+    private HashSet<String> whitelist;
+    private HashSet<String> blacklist;
 
     public SpamFilter(List<String> whitelist, List<String> blacklist) throws IllegalArgumentException {
         if (whitelist == null || blacklist == null) {
             throw new IllegalArgumentException();
         }
-
-        this.whitelist = whitelist;
-        this.blacklist = blacklist;
+        this.whitelist = new HashSet<>(whitelist);
+        this.blacklist = new HashSet<>(blacklist);
     }
 
     public List<List<String>> applyRules(List<List<String>> sentence) {
         List<List<String>> passedSentence = new ArrayList<>();
         List<List<String>> tempSentence = new ArrayList<>();
 
-        for (List<String> strings : sentence) {
-            for (String badWord : blacklist) {
-                if (strings.contains(badWord)) {
-                    break;
-                } else {
-                    tempSentence.add(strings);
+        if (blacklist.size() > 0) {
+            for (List<String> strings : sentence) {
+                for (String badWord : blacklist) {
+                    if (strings.contains(badWord)) {
+                        break;
+                    } else {
+                        tempSentence.add(strings);
+                    }
                 }
             }
+        }else{
+            tempSentence = passedSentence;
         }
-        if (tempSentence.size() != 0) {
+
+        if (tempSentence.size() > 0) {
             for (List<String> strings : tempSentence) {
                 for (String goodWord : whitelist) {
                     if (tempSentence.contains(goodWord)) {
