c47700b0fb503e24c297a0cda7b601325b39864a
Sziku <105989158+Sziku@users.noreply.github.com>
2022-12-13 10:36:15 +0100

    add methods run tests 5 passed

--- src/main/java/hu/nive/ujratervezes/spamfiltering/SpamFilter.java
+++ src/main/java/hu/nive/ujratervezes/spamfiltering/SpamFilter.java
@@ -3,5 +3,36 @@ package hu.nive.ujratervezes.spamfiltering;
 import java.util.*;
 
 public class SpamFilter {
+    private  List<String>  whitelist;
+    private  List<String>  blacklist;
 
+    public SpamFilter(List<String> whitelist, List<String> blacklist) throws IllegalArgumentException{
+        if(whitelist == null || blacklist == null){
+            throw new IllegalArgumentException();
+        }
+
+        this.whitelist = whitelist;
+        this.blacklist = blacklist;
+    }
+
+    public List<List<String>> applyRules(List<List<String>> sentence){
+        List<List<String>> passedSentence = new ArrayList<>();
+
+        for (List<String> strings : sentence) {
+            for (String badWord : blacklist) {
+                if (strings.contains(badWord)) {
+                    break;
+                }
+            }
+
+            for (String goodWord : whitelist) {
+                if (strings.contains(goodWord)) {
+                    passedSentence.add(strings);
+                    break;
+                }
+            }
+
+        }
+        return passedSentence;
+    }
 }
